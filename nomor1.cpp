e5797661ca389c461e859a153a963e797a3e180e
Muh. Firdaus <93996105+muhfirdaus08@users.noreply.github.com>
2022-05-31 20:47:21 +0800

    modul 4 function

--- nomor1.cpp
+++ nomor1.cpp
@@ -1,28 +1,378 @@
 #include <iostream>
-#include<iomanip>
 using namespace std;
 
-int main(){
-    int input, a = 5;
-    cout<<"Input : "; cin>>input;
-    for (int i = 0; i < input; i++){
-        for (int j = 0; j < input; j++){
-            // a = i-j;
-            // //atau a = j-i;
-            // if (a>0){
-            //     cout<< setw(3) << a;
-            // }
-            // else{
-            //     a = a*(-1);
-            //     cout<< setw(3) << a;
-            // }
+string username = "", password = "";
+string username1 = "", password1 = "";
+int pilihan = 0;
+int saldo = 1000000;
+int listrik = 150000, air = 100000, online = 200000;
 
-            if (i==0 && j>=0 && j<=input-1 || j==0 && i>=0 && i<=input-1 || i==input-1 && j>=0 && j<=input-1){
-                cout << a << " ";
-            }else {
-                cout << " ";
-            }
+
+void login();
+void masuk();
+void menu();
+void profil();
+void cekProfil();
+void yesno();
+void ubahUsername();
+void ubahPassword();
+void ubahPassword();
+void cekSaldo();
+void topUp();
+void transfer();
+void pembayaran();
+void pembonline();
+void pemblistrik();
+void pembair();
+
+int main(int argc, char const *argv[])
+{
+    login();
+    return 0;
+}
+
+void masuk() {
+    cout << "\nUsername anda : ";
+    cin >> username;
+    cout << "Password anda : ";
+    cin >> password;
+    
+    if ((username == username1) && (password == password1)){
+        cout << "Login berhasil, pilih menu di bawah ini" << endl;
+        menu();
+    }
+    else {
+        cout << "Username atau Password Salah atau Tidak Terdaftar" << endl;
+        login ();
+    }
+} 
+
+void daftar() {
+    cout << "\nSelamat Datang di Menu Daftar" << endl;
+    cout << "Username anda : ";
+    cin >> username1;
+    cout << "Password anda : ";
+    cin >> password1;
+    cout << "Terima Kasih, Silahkan Login" << endl;
+    login();
+}
+
+void login() {
+    while (pilihan != 3)
+    {
+        cout << "1. Masuk" << endl;
+        cout << "2. Daftar" << endl;
+        cout << "3. Keluar" << endl;
+        cout << "Pilih : ";
+        cin >> pilihan;
+
+        switch (pilihan){
+            case 1 : 
+                masuk();
+                break;
+            case 2 : 
+                daftar();
+                break;
+            case 3 :
+                cout << "Sampai Jumpa :)" << endl;
+                exit(0);
+                break;       
         }
-        cout<<endl;
+    }  
+}
+
+void cekProfil(){
+    cout << "1. Cek Username" << endl;
+    cout << "2. Cek Password" << endl;
+    cout << "3. Kembali" << endl;
+    cout << "Pilih : ";
+    cin >> pilihan;
+
+    switch (pilihan){
+        case 1 : 
+            cout << "Username anda : " << username1 << endl;
+            break;
+        case 2 :
+            cout << "Password anda : " << password1 << endl;
+            break;
+        case 3 :
+            menu();
+            break;
+    }
+    profil();
+}
+
+void yesno (){
+    cout << "Lanjutkan Perubahan ? Y/N : ";
+    string yn;
+    cin >> yn;
+
+    if ((yn == "Y")|| (yn == "y")){
+        cout << "Perubahan Berhasil" << endl;
+    }   
+    else if ((yn == "N")|| (yn == "n")){
+        cout << "Perubahan Gagal" << endl;
+    }
+}
+
+void ubahUsername(){
+    cout << "Masukan Username Baru : ";
+    cin >> username1;
+    username = username1;
+    yesno();
+}
+
+void ubahPassword(){
+    cout << "Masukan Password Baru : ";
+    cin >> password1;
+    password = password1;
+    yesno();
+}
+    
+void ubahProfil(){
+    cout << "1. Ubah Username" << endl;
+    cout << "2. Ubah Password" << endl;
+    cout << "3. Kembali" << endl;
+    cout << "Pilih : ";
+    cin >> pilihan;
+
+    switch (pilihan){
+        case 1 : 
+            ubahUsername();
+            break;
+        case 2 :
+            ubahPassword();
+            break;
+        case 3 :
+            menu();
+            break;
+    }
+    profil();
+}
+
+void profil(){
+    cout << "1. Cek Profil" << endl;
+    cout << "2. Ubah Profil" << endl;
+    cout << "3. Kembali" << endl;
+    cout << "Pilih : ";
+    cin >> pilihan;
+    switch (pilihan){
+        case 1 : 
+            cekProfil();
+            break;
+        case 2 :
+            ubahProfil();
+            break;
+        case 3 :
+            menu();
+            break;
+    }
+    profil();
+}
+
+void cekSaldo(){
+    cout << "Saldo Anda Adalah = " << saldo << endl;
+    menu();
+}
+
+void topUp (){
+    cout << "1. Lanjutkan TopUp" << endl;
+    cout << "2. Kembali" << endl;
+    cout << "Pilih = ";
+    cin >> pilihan;
+    cout << "\n";
+    
+    switch (pilihan){
+        case 1 :
+            cout << "Masukan Jumlah Saldo : ";
+            int jumlah;
+            cin >> jumlah;
+    
+            saldo = jumlah + saldo;
+            
+            cout << "Saldo Anda Sekarang Adalah = " << saldo << endl;
+            menu();
+            break;
+            
+        case 2 :
+            menu();
+            break;
+    }    
+}
+
+void tranfer (){
+    int idPenerima =0, nominal=0;
+    cout << "1. Lanjutkan Transfer" << endl;
+    cout << "2. Kembali" << endl;
+    cout << "Pilih = ";
+    cin >> pilihan;
+    cout << "\n";
+    
+    switch (pilihan){
+        case 1 :
+            if (saldo == 0){
+                cout << "Saldo Anda Kosong, Silahkan TopUp" << endl;
+            }
+            else {
+                cout << "Masukan No. ID Penerima : ";
+                cin >> idPenerima;
+                cout << "Masukan Nominal         : ";
+                cin >> nominal;
+                cout << "Transfer Berhasil";
+
+                saldo = saldo - nominal;
+
+                cout << "No. ID Penerima        : " << idPenerima << endl;
+                cout << "Nominal                : " << nominal << endl;
+                cout << "Sisa Saldo Ando Adalah = " << saldo << endl;
+            }
+            menu();
+            break;
+        case 2 :
+            menu();
+            break;
+    }
+                
+}
+
+void pemblistrik (){
+    cout << "Tagihan Listrik Anda  : " << listrik << endl;
+    cout << "Lanjutkan Pembayaran ? Y/N : ";
+    string yn;
+    cin >> yn;
+
+    if ((yn == "Y")|| (yn == "y")){
+        if (saldo < listrik){
+            cout << "Saldo Tidak Cukup, Silahkan TopUp" << endl;
+            menu();
+        }
+        else {
+            saldo = saldo - listrik;
+            cout << "Pembayaran Berhasil" << endl;
+        }  
+    }   
+    else if ((yn == "N")|| (yn == "n")){
+        cout << "Pembayaran Gagal" << endl;
+    }
+    cout << "Sisa Saldo Anda " << saldo << endl;
+    menu();
+}
+
+void pembair (){
+    cout << "Tagihan Air Anda      : " << air << endl;
+    cout << "Lanjutkan Pembayaran ? Y/N : ";
+    string yn;
+    cin >> yn;
+
+    if ((yn == "Y")|| (yn == "y")){
+        if (saldo < air){
+            cout << "Saldo Tidak Cukup, Silahkan TopUp" << endl;
+            menu();
+        }
+        else {
+            saldo = saldo - listrik;
+            cout << "Pembayaran Berhasil" << endl;
+        }  
+    }   
+    else if ((yn == "N")|| (yn == "n")){
+        cout << "Pembayaran Gagal" << endl;
+    }
+    cout << "Sisa Saldo Anda " << saldo << endl;
+    menu();
+}
+
+void pembonline (){
+    cout << "Tagihan Belanja Online Anda : " << online;
+    cout << "Lanjutkan Pembayaran ? Y/N : ";
+    string yn;
+    cin >> yn;
+
+    if ((yn == "Y")|| (yn == "y")){
+        if (saldo < online){
+            cout << "Saldo Tidak Cukup, Silahkan TopUp" << endl;
+            menu();
+        }
+        else {
+            saldo = saldo - listrik;
+            cout << "Pembayaran Berhasil" << endl;
+        }  
+    }   
+    else if ((yn == "N")|| (yn == "n")){
+        cout << "Pembayaran Gagal" << endl;
+    }
+    cout << "Sisa Saldo Anda " << saldo << endl;
+    menu();
+}
+
+
+void pembayaran (){
+    cout << "1. Listrik" << endl;
+    cout << "2. Air" << endl;
+    cout << "3. Belanja Online" << endl;
+    cout << "4. Kembali" << endl;
+    cout << "Pilih = ";
+    cin >> pilihan;
+    cout << "\n";
+    
+    switch (pilihan){
+        case 1 :
+            cout << "Masukan Nomor Pelanggan : ";
+            int nomor;
+            cin >> nomor;
+            cout << "Nomor Pelanggan Anda  : " << nomor << endl;
+            pemblistrik();
+            break;
+            
+        case 2 :
+            cout << "Masukan Nomor Pelanggan : ";
+            cin >> nomor;
+            cout << "Nomor Pelanggan Anda  : "<< nomor << endl;
+            pembair();
+            break;
+            
+        case 3 :
+            cout << "Masukan Nomor Pembayaran   : ";
+            cin >> nomor;
+            cout << "Nomor Pembayaran Anda    : "<< nomor << endl;
+            pembonline();
+            break;
+            
+        case 4 :
+            menu();
+            break;
+    }
+    pembayaran();
+}
+
+void menu (){
+    cout << "1. Cek Saldo" << endl;
+    cout << "2. Top Up Saldo" << endl;
+    cout << "3. Transfer" << endl;
+    cout << "4. Pembayaran" << endl;
+    cout << "5. Profil" << endl;
+    cout << "6. Logout" << endl;
+    cout << "Pilih : ";
+    cin >> pilihan;
+    cout << "\n";
+    
+    switch (pilihan){
+        case 1 :
+            cekSaldo();
+            break;
+        case 2 :
+            topUp();
+            break;
+        case 3 :
+            tranfer();
+            break;
+        case 4 :
+            pembayaran();
+            break;
+        case 5 :
+            profil();
+            break;
+        case 6 :
+            login();
+            break;         
     }
 }
--- nomor2.cpp
+++ nomor2.cpp
@@ -1,66 +1,138 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int a, b, n, jumlah = 0;
+//variabel global
+int a;
 
-    cout << "Masukin nilai akhir (x) : ";
-    cin >> a;
-    cout << "Masukin selish (y) : ";
-    cin >> b;
-    cout << "Masukin banyak barisan(n) : ";
-    cin >> n;
+int max_value(int matriks[][100], int baris, int kolom);
+void compare_max(int matriks_pertama[][100], int matriks_kedua[][100]);
+int min_value(int matriks[][100], int baris, int kolom);
+void compare_min(int matriks_pertama[][100], int matriks_kedua[][100]);
+void print_matriks(int matriks[][100], int baris, int kolom);
 
-    // aritmatika
 
-    cout << "\nSeluruh bilangan aritmatika : ";
-    for (int i=a+((n-1)*b); i>=a; i-=b){
-        cout << i << " ";
-        jumlah += i;
-    }
-    cout << endl << "Jumlah : " << jumlah << endl;
-    jumlah = 0; 
+int main(int argc, char const *argv[])
+{   
+	//variabel local
+	int matriks1 [100][100];
+	int matriks2 [100][100];
 
-    //prima
+	//input ordo
+	printf("\n\nInput ordo matriks persegi = ");
+	scanf("%d", &a);
 
-    cout << "\nSeluruh bilangan prima : ";
-    for (int i=a+((n-1)*b); i>=a; i-=b){
-        int hitung = 0;
-        for (int j=1; j<=i; j++){
-            if (i%j == 0){
-                hitung++;
-            }
-        }
-        if (hitung == 2){
-            cout << i << " ";
-            jumlah += i;
-        }          
-    }
-    cout << endl << "Jumlah : " << jumlah << endl;
-    jumlah = 0; 
-
-    //fibonanci
-
-    cout << "\nSeluruh bilangan fibonanci : ";
-    for (int i=a+((n-1)*b); i>=a; i-=b){
-        int angka1 = 0; 
-        int angka2 = 1;
-        for (int j=i; j>0; j--){
-            int sum = angka1 + angka2;
-            angka1 = angka2;
-            angka2 = sum;
-            if (sum == i){
-                cout << sum << " ";
-                if (i==1){
-                    cout << a << " "; 
-                }
-                jumlah = jumlah += sum;
-            }
+	//input matriks 1
+    printf("\nInput nilai matriks pada matriks 1\n");
+    for (int row = 0; row < a; row++) {
+        for (int column = 0; column < a; column++) {
+            printf("Matriks 1 [%d][%d] = ", row, column);
+            scanf("%d", &matriks1[row][column]);
         }
     }
-    if (a == 1){
-        jumlah += a;
+
+
+    //input matriks 2
+    printf("\nInput nilai matriks pada matriks 2\n");
+    for (int row = 0; row < a; row++) {
+        for (int column = 0; column < a; column++) {
+            printf("Matriks 2 [%d][%d] = ", row, column);
+            scanf("%d", &matriks2[row][column]);
+        }
     }
-    cout << endl << "Jumlah : " << jumlah << endl;
-    return 0;
+
+	//output
+	printf("\nHasil input matriks :\n");
+    printf("\nMatriks 1\n");
+	print_matriks(matriks1, a, a);
+    printf("Matriks 2\n");
+	print_matriks(matriks2, a, a);
+
+	printf("\n\n===Hasil Penentuan Angka Terbesar dan Terkecil Pada Dua Matriks==\n");
+	printf("\nAngka terbesar dari Matriks 1 dan Matriks 2 adalah '");
+	compare_max(matriks1, matriks2);
+	printf("\nAngka terkecil dari Matriks 1 dan Matriks 2 adalah '");
+	compare_min(matriks1, matriks2);
+
+	printf("\n\n");
+
+	return 0;
 }
+
+
+void print_matriks(int matriks[][100], int baris, int kolom)
+{
+    for (int row = 0; row < baris; row++) {
+		printf("[");
+        for (int column = 0; column < kolom; column++) {
+            printf("%d", matriks[row][column]);
+			if (column < (kolom - 1)){
+				printf(",");
+			}else {
+				printf("]");
+			}
+		}
+        printf("\n");
+    }
+	printf("\n");
+}
+
+int max_value(int matriks[][100], int baris, int kolom)
+{
+    int column = 0, row = 0;
+    int max = matriks[0][0];
+
+    for (row = 0; row < baris; row++) {
+        for (column = 0; column < kolom; column++) {
+            if (matriks[row][column] > max) {
+                max = matriks[row][column];
+            }
+        }
+    }
+    return max;
+}
+
+void compare_max(int matriks_pertama[][100], int matriks_kedua[][100])
+{
+	int max_matriks1 = max_value(matriks_pertama, a, a);
+	int max_matriks2 = max_value(matriks_kedua, a, a);
+
+	if (max_matriks1 > max_matriks2){
+		printf("%d' yang berada pada Matriks 1", max_matriks1);
+	}else if (max_matriks1 < max_matriks2){
+		printf("%d' yang berada pada Matriks 2", max_matriks2);
+	}else if (max_matriks1 == max_matriks2){
+		printf("%d' yang berada pada Matriks 1 dan 2", max_matriks1);
+	}
+}
+
+
+int min_value(int matriks[][100], int baris, int kolom)
+{
+    int column = 0, row = 0;
+    int min = matriks[0][0];
+
+    for (row = 0; row < baris; row++) {
+        for (column = 0; column < kolom; column++) {
+            if (matriks[row][column] < min) {
+                min = matriks[row][column];
+            }
+        }
+    }
+    return min;
+}
+
+void compare_min(int matriks_pertama[][100], int matriks_kedua[][100])
+{
+	int min_matriks1 = min_value(matriks_pertama, a, a);
+	int min_matriks2 = min_value(matriks_kedua, a, a);
+
+	if (min_matriks1 < min_matriks2){
+		printf("%d' yang berada pada Matriks 1", min_matriks1);
+	}else if (min_matriks1 > min_matriks2){
+		printf("%d' yang berada pada Matriks 2", min_matriks2);
+	}else if (min_matriks1 == min_matriks2){
+		printf("%d' yang berada pada Matriks 1 dan 2", min_matriks1);
+	}
+}
+
+
